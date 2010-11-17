7eba7378d1057535786b7d4c9ac880a5bfa7218f
Yusuke Wada <yusuke@kamawada.com>
2010-11-17 11:01:33 +0900

    initial commit

---
+++ lib/Acme/Zenra.pm
@@ -0,0 +1,32 @@
+package Acme::Zenra;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::Zenra -
+
+=head1 SYNOPSIS
+
+  use Acme::Zenra;
+
+=head1 DESCRIPTION
+
+Acme::Zenra is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
